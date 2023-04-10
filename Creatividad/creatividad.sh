e62716437bba0eac4d3994d762fa6942573c9d5c
cristian2501 <mwarnes22@jaumebalmes.net>
2023-04-10 19:45:50 +0200

    Creatividad prueba 2

--- Creatividad/creatividad.sh
+++ Creatividad/creatividad.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
-
-LINES=$(tput lines) COLUMNS=$(tput cols)
-
-declare -A snowflakes declare -A lastflakes
-
-clear
-
-function move_flake() { i=”$1″
-
-if [ "${snowflakes[$i]}” = “” ] || [ "${snowflakes[$i]}” = “$LINES” ]; then snowflakes[$i]=0 else if [ "${lastflakes[$i]}” != “” ]; then printf “\033[%s;%sH \033[1;1H " ${lastflakes[$i]} $i fi fi
-
-printf “\033[%s;%sH❄\033[1;1H" ${snowflakes[$i]} $i
-
-lastflakes[$i]=${snowflakes[$i]} snowflakes[$i]=$((${snowflakes[$i]}+1)) }
-
-while : do i=$(($RANDOM % $COLUMNS))
-
-move_flake $i
-
-for x in “${!lastflakes[@]}” do move_flake “$x” done
-
-sleep 0.1 done [/bash]
+Defecto = '\e[39m'
+Amarillo = '\e[33m'
+Bold = '\e[1m'
+Rojo = '\e[31m'  
+subrayado = '\e[4m'
+Parpadeo: \e[5m
+Azul: '\e[34m'
+Verde: \e[32m
+Texto rojo + Fondo azul: \e;31;44m
+Negrita + Texto amarillo + Fondo azul: \e[1;33;44m
+#Códigos de color de fondo
+FRojo: \e[41m
+FVerde: \e[42m
+FAmarillo: \e[43m
+FAzul: \e[44m
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e " ${Amarillo}${Bold}_____${Defecto} "
+echo -e " ${Amarillo}${Bold}❏❏❏❏❏${Defecto} "
+echo -e " ${Amarillo}${Bold}❏❏❏❏❏${Defecto} "
+echo -e " ${Amarillo}${Bold}❏❏❏❏❏${Defecto} "
+echo -e " ${Amarillo}${Bold}❏❏❏❏❏${Defecto} "
+echo " ${Amarillo}${Bold}❏❏${Defecto} "
+echo -e " ${Amarillo}${Bold}❏❏❏❏❏${Defecto} "
+echo ""
+echo ""
+echo ""
+echo ""
